c2bed80b32479327a49b94200408176cfb211b58
manmanher <manmanher@alum.us.es>
2023-05-11 11:40:00 +0200

    Issues 86 y 90

---
+++ src/main/java/us/muit/fs/a4i/model/entities/Font.java
@@ -0,0 +1,16 @@
+package us.muit.fs.a4i.model.entities;
+
+public class Font extends java.awt.Font{
+	
+	private String color;
+
+	public Font(String name, int size, String color){
+		super(name, PLAIN, size);
+		this.color = color;
+	}
+	
+	public String getColor(){
+		return this.color;
+	}
+	
+}
